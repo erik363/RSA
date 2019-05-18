c181dc67e8f9dded88eb7984444a3141776f3982
Erik <erik3630@gmail.com>
2019-05-18 21:55:04 +0200

    euklidesz

--- venv/Include/rya.py
+++ venv/Include/rya.py
@@ -9,4 +9,28 @@ def gyorshatvanyozas(a, n, m):
     mod *= actual
     return mod%m
 
+def euklidesz(r1, r2):
+    q = r1//r2
+    x1 = 1
+    x2 = 0
+    y1 = 0
+    y2 = 1
+    temp = 0
+    tempx = 0
+    tempy = 0
+    while(r2 != 0):
+        temp = r2
+        q = r1 // r2
+        r2 = r1%r2
+        r1 = temp
+        tempx = x2
+        x2 = x2*q + x1
+
+        tempy = y2
+        x1 = tempx
+        y2 = y2*q + y1
+        y1 = tempy
+    return r1, q, x1, y1
+
 print(gyorshatvanyozas(23, 209, 211))
+print(euklidesz(141, 17))
